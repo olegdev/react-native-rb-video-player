032ad6879676a4438ce9c77baff178faeb47984c
Oleg Mikhailov <s2ferr@gmail.com>
2019-09-27 10:46:04 +0300

    fullscreen fix

--- components/ControlBar.js
+++ components/ControlBar.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { StyleSheet } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { ToggleIcon, Time, Scrubber } from "./";
+import { isIphoneX } from "./utils";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,6 +11,15 @@ const styles = StyleSheet.create({
     height: 35,
     alignSelf: "stretch",
     justifyContent: "flex-end"
+  },
+  fullscreen: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 30
+  },
+  iPhoneXStyle: {
+    bottom: 100
   }
 });
 
@@ -30,7 +40,11 @@ const ControlBar = props => {
   return (
     <LinearGradient
       colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.75)"]}
-      style={styles.container}
+      style={[
+        styles.container,
+        fullscreen && styles.fullscreen,
+        isIphoneX() && fullscreen && styles.iPhoneXStyle
+      ]}
     >
       <Time time={currentTime} theme={theme.seconds} />
       <Scrubber
@@ -52,7 +66,7 @@ const ControlBar = props => {
         size={20}
       />
       <Time time={duration} theme={theme.duration} />
-      {(!inlineOnly || !hideFullScreenControl) && (
+      {!inlineOnly && !hideFullScreenControl && (
         <ToggleIcon
           paddingRight
           onPress={() => props.toggleFS()}
